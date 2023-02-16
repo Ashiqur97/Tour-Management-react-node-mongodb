1940727606f8318e4251d4f230abe619b2741ee3
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-02-17 01:25:19 +0600

    Mern Stack Project

---
+++ frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry'
+import galleryImages from "./galleryImages";
+
+const MasonryImagesGallery = () => {
+    return (
+        <ResponsiveMasonry>
+            <Masonry gutter="1rem">
+                {galleryImages.map((item,index) =>(
+                    <img className="masonry__img" 
+                    src={item}
+                    key={index}
+                    alt=""
+                    style={{width: "100%", display: "block", borderRadius: "10px"}} />
+                ))}
+            </Masonry>
+        </ResponsiveMasonry>
+    )
+}
+
+export default MasonryImagesGallery
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,9 +7,9 @@ import heroImg02 from '../assets/images/hero-img02.jpg'
 import heroVideo from '../assets/images/hero-video.mp4'
 import worldImg from '../assets/images/world.png'
 import Subtitle from '../shared/Subtitle'
-
+import exprienceImg from '../assets/images/experience.png'
 import SearchBar from '../shared/SearchBar';
-
+import MasonryImagesGallery from '../components/Image-gallery/MasonryImagesGallery'
 import ServiceList from '../services/ServiceList'
 
 import FeaturedTourList from '../components/Feature-tours/FeaturedTourList'
@@ -66,7 +66,6 @@ const Home = () => {
       <ServiceList />
     </Row>
   </Container>
-
 </section>
    
    <section>
@@ -77,10 +76,62 @@ const Home = () => {
           <h2 className='featured__tour-title'>Our feature tours</h2>
         </Col>
         <FeaturedTourList />
+        <section>
+          <Container>
+            <Row>
+              <Col lg='6'>
+                <div className='experience__content'>
+                    <Subtitle subtitle={'Exprience'} />
+
+                    <h2>With our all exprience <br/> we will serve you</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                      <br />
+                    Soluta totam ipsam esse cum voluptatem autem molestiae quasi ratione quos nostrum</p>
+                </div>
+                <div className='counter__wrapper d-flex align-items-center gap-5'>
+                    <div className='counter__box'>
+                        <span>12k+</span>
+                        <h6>Successful Trip</h6>
+                    </div>
+
+                    <div className='counter__box'>
+                        <span>12k+</span>
+                        <h6>Regular Clients</h6>
+                    </div>
+
+                    <div className='counter__box'>
+                        <span>12k+</span>
+                        <h6>Years exprience</h6>
+                    </div>
+                </div>
+              </Col>
+              <Col lg='6'>
+                  <div className='exprience__img'>
+                      <img src={exprienceImg} alt="" />
+                  </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
       </Row>
     </Container>
    </section>
 
+<section>
+  <Container>
+    <Row>
+      <Col lg='12'>
+        <Subtitle subtitle={"Gallery"} />
+          <h2 className='gallery__title'>
+            Visit our customers tour gallery
+          </h2>
+      </Col>
+      <Col lg="12">
+          <MasonryImagesGallery />
+      </Col>
+    </Row>
+  </Container>
+</section>
     </>
   )
 }
