1fc38900ebf3c8d7ca8836dd6d45745a81875467
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-02-19 23:19:26 +0600

    Mern Stack Project

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './booking.css'
 import {Form,FormGroup,ListGroup,ListGroupItem,Button} from 'reactstrap';
+import {useNavigate} from "react-router-dom";
 
 const Booking = ({tour,avgRating}) => {
     const {price,reviews} = tour;
+    const navigate = useNavigate()
+
+
+        const [credentials, setCredentials] = useState({
+            userId: '01',
+            userEmail: 'example@gmail.com',
+            fullName: '',
+            phone: '',
+            guestSize:1,
+            bookAt:''
+        })
 
     const handleChange = e => {
-
+        setCredentials(prev=>({...prev, [e.target.id]:e.target.value}))
     }
+
+    const serviceFee = 10
+    const totalAmount = Number(price) * Number(credentials.guestSize) + Number(serviceFee)
+
+    const handleClick = e=>{
+        e.preventDefault()
+
+       navigate("/thank-you");
+    }
+
   return (
     <div className='booking'>
         <div className='booking__top d-flex align-items-center justify-content-between'>
@@ -20,13 +42,14 @@ const Booking = ({tour,avgRating}) => {
 
         <div className='booking__form'>
             <h5>Information</h5>
-            <Form className='booking__info-form'>
+            <Form className='booking__info-form' onSubmit={handleClick}>
                 <FormGroup>
                     <input 
                     type="text" 
                     placeholder='Full Name' 
                     id="fullName" 
-                    required onChange={handleChange} />
+                    required 
+                    onChange={handleChange} />
                 </FormGroup>
 
                 <FormGroup>
@@ -34,7 +57,8 @@ const Booking = ({tour,avgRating}) => {
                     type="number" 
                     placeholder='Phone' 
                     id="phone" 
-                    required onChange={handleChange} />
+                    required 
+                    onChange={handleChange} />
                 </FormGroup>
 
                 <FormGroup className='d-flex align-items-center gap-3'>
@@ -42,15 +66,39 @@ const Booking = ({tour,avgRating}) => {
                     type="date" 
                     placeholder='' 
                     id="bookAt" 
-                    required onChange={handleChange} />
+                    required 
+                    onChange={handleChange} />
 
                     <input 
                     type="text" 
                     placeholder='Guest' 
                     id="guestSize" 
-                    required onChange={handleChange} />
+                    required 
+                    onChange={handleChange} />
                 </FormGroup>
             </Form>
+        </div>
+        <div className='booking'>
+            <ListGroup>
+                <ListGroupItem className='border-0 px-0'>
+                <h5 className='d-flex align-items-center gap-1'>
+                    ${price} <i class="ri-close-circle-line"></i>
+                </h5>
+                <span>${price}</span>
+                </ListGroupItem>
+
+                <ListGroupItem className='border-0 px-0'>
+                <h5>Service charge <i class="ri-close-circle-line"></i></h5>
+                <span>${serviceFee}</span>
+                </ListGroupItem>
+
+                <ListGroupItem className='border-0 px-0'>
+                <h5>Total <i class="ri-close-circle-line"></i></h5>
+                <span>${totalAmount}</span>
+                </ListGroupItem>
+            </ListGroup>
+
+            <Button className='btn primary__btn w-100 mt-4' onClick={handleClick}>Book Now</Button>
         </div>
     </div>
   )
--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -6,7 +6,7 @@ import { Container,Row,Col, ListGroup,Form } from 'reactstrap';
 import calculateAvgRating from './../utils/avgRating';
 import avatar from '../assets/images/avatar.jpg';
 import Booking from '../components/Booking/Booking';
-
+import Newsletter from '../shared/Newsletter'
 
 const TourDetails = () => {
   const {id} = useParams()
@@ -127,6 +127,7 @@ const TourDetails = () => {
           </Row>
         </Container>
       </section>
+      <Newsletter />
     </>
   )
 }
