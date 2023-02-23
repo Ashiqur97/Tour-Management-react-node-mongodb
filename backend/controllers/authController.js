d91b155657474ae18782bcb7cc5d412d37822eb9
Ashiqur97 <ashiqurrahmanabir96@gmail.com>
2023-02-24 04:55:42 +0600

    Mern Stack Project

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,10 +4,15 @@ import jwt from 'jsonwebtoken';
 
 export const register = async (req,res) => {
     try{
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password,salt);
+
+
         const newUser = new User({
             username: req.body.username,
             email:req.body.email,
-            password:req.body.password,
+            password:hash,
             photo:req.body.photo,
         });
 
@@ -21,9 +26,35 @@ export const register = async (req,res) => {
 };
 
 export const login = async(req,res) =>{
+    const email = req.body.email
+
     try {
+        const user = await User.findOne({email})
         
+        if(!user){
+            return res.status(404).json({success:false,message:"User not found"})
+        }
+
+        const checkCorrectPasword = bcrypt.compare(req.body.password, user.password)
+
+        if(!checkCorrectPasword) {
+            return res.status(401).json({success:false, message:"Incorrect email or password"})
+        }
+
+        const {password,role, ...rest} = user._doc;
+        
+        const token = jwt.sign(
+            {id: user._id, role: user.role},
+            process.env.JWT_SECRET_KEY,
+            {expiresIn: "15d"}
+        );
+
+        res.cookie('accessToken',token,{
+            httpOnly:true,
+            expires:token.expiresIn
+        }).status(200).json({success:true, message:'successfully login', data:{...rest}})
+
     } catch (err) {
-        
+        res.status(500).json({success:false,message:"Failed to login"});
     }
 };
